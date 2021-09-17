7fd645bd061e2a441fbd6aa2363d552305143f42
Dion Timmermann <dion.timmermann@ptb.de>
2021-09-17 10:35:36 +0200

    Updated Header information

--- @MCProp/MCProp.m
+++ @MCProp/MCProp.m
@@ -1,6 +1,6 @@
 % Metas.UncLib.Matlab.MCProp V2.4.9
 % Michael Wollensack METAS - 05.08.2021
-% Dion Timmermann PTB - 16.09.2021
+% Dion Timmermann PTB - 17.09.2021
 %
 % MCProp Const:
 % a = MCProp(value)

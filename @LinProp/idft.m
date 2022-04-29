ca3e5ed505d4c3a98d046b2bc15e4c026a438a5d
Michael Wollensack <22192118+wollmich@users.noreply.github.com>
2022-04-29 10:08:06 +0200

    Local Convert2UncArray and Convert2LinProp functions removed.

--- @LinProp/idft.m
+++ @LinProp/idft.m
@@ -12,40 +12,11 @@ function X = idft(A, varargin)
     end
     A = complex(A);
     s = size(A);
-    am = Convert2UncArray(A);
+    am = LinProp.Convert2UncArray(A);
     xm = numlib.Idft(am, realTimeDomainData);
-    X = Convert2LinProp(xm);
+    X = LinProp.Convert2LinProp(xm);
     if (realTimeDomainData)
         X = real(X);
     end
     X = reshape(X, s);
-end
-
-function m = Convert2UncArray(x)
-    if x.IsArray
-        m = x.NetObject;
-    else
-        if x.IsComplex
-            m = NET.createGeneric('Metas.UncLib.Core.Ndims.ComplexNArray', {'Metas.UncLib.LinProp.UncNumber'});
-        else
-            m = NET.createGeneric('Metas.UncLib.Core.Ndims.RealNArray', {'Metas.UncLib.LinProp.UncNumber'});
-        end
-        m.Init2d(1, 1);
-        m.SetItem2d(0, 0, x.NetObject);
-    end 
-end
-
-function u = Convert2LinProp(x)
-    if LinProp.IsArrayNet(x)
-        if x.numel == 1
-            u = LinProp(x.GetItem2d(0, 0));
-        else
-            u = LinProp(x);
-            if ndims(u) == 1
-                u = reshape(u, size(u));
-            end
-        end
-    else
-        u = LinProp(x);
-    end
 end

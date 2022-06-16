cd2a2c625af230d28a6940b9eb25ab362466dc88
Dion Timmermann <dion.timmermann@ptb.de>
2022-06-16 12:44:39 +0200

    Added static method empty().

--- @MCProp/MCProp.m
+++ @MCProp/MCProp.m
@@ -1,6 +1,6 @@
 % Metas.UncLib.Matlab.MCProp V2.5.4
 % Michael Wollensack METAS - 10.05.2022
-% Dion Timmermann PTB - 01.06.2022
+% Dion Timmermann PTB - 16.06.2022
 %
 % MCProp Const:
 % a = MCProp(value)
@@ -490,7 +490,7 @@ classdef MCProp
                 s = double(varargin{1});
             end
             if numel(s) < 2
-                error('Size vector must have at least two elements.');
+                error('MATLAB:getReshapeDims:sizeVector', 'Size vector must have at least two elements.');
             end
             if any(not(isreal(s)))
                 error('Size argument cannot be complex.');
@@ -508,7 +508,7 @@ classdef MCProp
                 end
             else
                 if prod(s) ~= numel(x)
-                    error('Number of elements must not change. Use [] as one of the size inputs to automatically calculate the appropriate size for that dimension.');
+                    error('MATLAB:getReshapeDims:notSameNumel', 'Number of elements must not change. Use [] as one of the size inputs to automatically calculate the appropriate size for that dimension.');
                 end
             end
             y = copy(x);
@@ -1950,6 +1950,22 @@ classdef MCProp
         end
         function x = randn(varargin)
             x = MCProp(randn(varargin{:}));
+        end
+        function x = empty(varargin)
+            try
+                x = reshape(MCProp([]), varargin{:});
+            catch e
+                switch (e.identifier)
+                    case 'MATLAB:getReshapeDims:notSameNumel'
+                        error('MATLAB:class:emptyMustBeZero', 'At least one dimension must be zero.');
+                    case 'MATLAB:getReshapeDims:sizeVector'
+                        % This error is triggered with MCProp.empty(0),
+                        % which should return a 0-by-0 element.
+                        x = MCProp([]);
+                    otherwise
+                        rethrow(e);
+                end
+            end
         end
     end
     properties (Constant, Access = private)

e0428adca5b254175df55821467eb278ffdf1792
Dion Timmermann <dion.timmermann@ptb.de>
2022-11-14 09:39:53 +0100

    Fixed definition of properties of Const2014_90

--- +MCProp/Const2014_90.m
+++ +MCProp/Const2014_90.m
@@ -2,19 +2,19 @@ classdef Const2014_90
 % Physical Constants CODATA 2014 for Conventional Electrical Units 90
     
     properties (Constant)
-        deltavCs =  MCProp.Const2014.data.deltavCs;    % Hyperfine transition frequency of Cs-133 / Hz
-        c0 =        MCProp.Const2014.data.c0;          % Speed of light in vacuum / (m/s)
-        mu0 =       MCProp.Const2014.data.mu0;         % Vacuum magnetic permeability / (Vs/Am)
-        ep0 =       MCProp.Const2014.data.ep0;         % Vacuum electric permittivity / (As/Vm)
-        Kcd =       MCProp.Const2014.data.Kcd;         % Luminous efficacy / (lm/W)
-        Mu =        MCProp.Const2014.data.Mu;          % Molar mass constant / (kg/mol)
-        Kj =        MCProp.Const2014.data.Kj;          % Conventional value of Josephson constant / (Hz/V)
-        Rk =        MCProp.Const2014.data.Rk;          % Conventional value of von Klitzing constant / Ohm
-        e =         MCProp.Const2014.data.e;           % Elementary charge / C
-        h =         MCProp.Const2014.data.h;           % Planck constant / Js
-        Na =        MCProp.Const2014.data.Na;          % Avogadro constant / (1/mol)
-        F =         MCProp.Const2014.data.F;           % Faraday constant / (C/mol)
-        k =         MCProp.Const2014.data.k;           % Boltzmann constant / (J/K)
+        deltavCs =  MCProp.Const2014_90.data.deltavCs;    % Hyperfine transition frequency of Cs-133 / Hz
+        c0 =        MCProp.Const2014_90.data.c0;          % Speed of light in vacuum / (m/s)
+        mu0 =       MCProp.Const2014_90.data.mu0;         % Vacuum magnetic permeability / (Vs/Am)
+        ep0 =       MCProp.Const2014_90.data.ep0;         % Vacuum electric permittivity / (As/Vm)
+        Kcd =       MCProp.Const2014_90.data.Kcd;         % Luminous efficacy / (lm/W)
+        Mu =        MCProp.Const2014_90.data.Mu;          % Molar mass constant / (kg/mol)
+        Kj =        MCProp.Const2014_90.data.Kj;          % Conventional value of Josephson constant / (Hz/V)
+        Rk =        MCProp.Const2014_90.data.Rk;          % Conventional value of von Klitzing constant / Ohm
+        e =         MCProp.Const2014_90.data.e;           % Elementary charge / C
+        h =         MCProp.Const2014_90.data.h;           % Planck constant / Js
+        Na =        MCProp.Const2014_90.data.Na;          % Avogadro constant / (1/mol)
+        F =         MCProp.Const2014_90.data.F;           % Faraday constant / (C/mol)
+        k =         MCProp.Const2014_90.data.k;           % Boltzmann constant / (J/K)
     end
     
     properties (Constant, Access = private)
